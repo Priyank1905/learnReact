26b91786a370febf40bc9cea50b4f41a2c360487
Priyank1905 <priyankjethva1905@gmail.com>
2025-01-19 11:56:48 +0530

    update Card component image; enhance ThemeBtn with theme toggle functionality and context integration

--- themeSwitcher/src/contexts/Theme.js
+++ themeSwitcher/src/contexts/Theme.js
@@ -1,13 +1,13 @@
 import { createContext, useContext } from "react";
 
-const ThemeContext = createContext({
+export const ThemeContext = createContext({
     themeMode: "light",
-    darkTheme: () => { },
-    lightTheme: () => { }
-});
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
 
-const ThemeProvider = ThemeContext.Provider;
+export const ThemeProvider = ThemeContext.Provider
 
-export default function useTheme() {
-    return useContext(ThemeContext);
+export default function useTheme(){
+    return useContext(ThemeContext)
 }
