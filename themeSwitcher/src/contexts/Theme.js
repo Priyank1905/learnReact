06c4b421f78e4eadbc639f98dd374b8b947a383f
Priyank1905 <priyankjethva1905@gmail.com>
2025-01-19 10:33:59 +0530

    initialize React app with Vite and Tailwind CSS setup

---
+++ themeSwitcher/src/contexts/Theme.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => { },
+    lightTheme: () => { }
+});
+
+const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+    return useContext(ThemeContext);
+}
