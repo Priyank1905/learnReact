9b5243e6f7dec58404b40542193b48fae71adec1
Priyank1905 <priyankjethva1905@gmail.com>
2025-01-19 10:57:49 +0530

    implement theme switching functionality with context provider

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { ThemeProvider } from './contexts/Theme'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => { 
+    setThemeMode("dark")
+  }
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark","light")
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode])
 
   return (
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
 
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          </div>
 
-        </div>
+          <div className="w-full max-w-sm mx-auto">
 
-        <div className="w-full max-w-sm mx-auto">
-
+          </div>
         </div>
       </div>
-    </div>
-
+    </ThemeProvider>
   )
 }
 
